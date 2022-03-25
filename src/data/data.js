75e61d56e96d730cf0736b2e8a5c8a80af74d08c
Linda Peng <lpnotes@Lindas-MacBook-Pro-2.local>
2022-03-25 15:56:04 -0700

    [ISSUE-22] Added more songs'

--- src/data/data.js
+++ src/data/data.js
@@ -1,4 +1,24 @@
 export const data = [
     { "sequence": ["D4", "E4", "C4", "A3", "B3", "G3"], "duration": [1, 1, 1, 2, 1, 2], song: "The Entertainer" },
-    { "sequence": ["C4", "C4", "G4", "G4", "A4", "A4"], "duration": [1, 1, 1, 1, 1, 1], song: "Twinkle Twinkle Little Star" }
+    { "sequence": ["C4", "C4", "G4", "G4", "A4", "A4"], "duration": [1, 1, 1, 1, 1, 1], song: "Twinkle Twinkle Little Star" },
+    { "sequence": ["E4", "A4", "C5", "B4", "A4", "E5"], "duration": [1, 2, 0.5, 0.5, 2, 1], song: "Harry Potter theme" },
+    { "sequence": ["A4", "D4", "F4", "G4", "A4", "D4"], "duration": [3, 3, 0.5, 0.5, 2, 2], song: "Game of Thrones" },
+    { "sequence": ["A4", "A4", "A3", "A3", "E4", "E4"], "duration": [1, 1, 1, 1, 1, 1], song: "Poker Face" },
+    { "sequence": ["G4", "G4", "G4", "A4", "B4", "B4"], "duration": [3, 3, 2, 1, 3, 2], song: "Row Row Row Your Boat" },
+    { "sequence": ["D4", "D4", "A4", "G4", "F4", "E4"], "duration": [2, 1, 1, 2, 2, 2], song: "Coffin Dance" },
+    { "sequence": ["E4", "A4", "G4", "A4", "B4", "C5"], "duration": [1, 2, 1, 2, 1, 2], song: "A Window to the Past" },
+    { "sequence": ["E4", "B4", "G4", "B4", "E4", "B4"], "duration": [1, 1, 1, 1, 1, 1], song: "Comptine d'un Autre été: L'Après-Midi" },
+    { "sequence": ["D3", "D4", "F4", "F4", "E4", "D4"], "duration": [4, 4, 1.5, 0.5, 1.5, 0.5], song: "Believer" },
+    { "sequence": ["F4", "E4", "D4", "A3", "F4", "E4"], "duration": [2, 2, 2, 2, 2, 2], song: "Requiem for a Dream" },
+    { "sequence": ["D4", "F4", "A4", "D5", "A4", "F4"], "duration": [2, 2, 2, 2, 2, 2], song: "Nothing Else Matters" },
+    { "sequence": ["E4", "E4", "G4", "E4", "D4", "C4"], "duration": [2, 1, 1, 1, 1, 3], song: "Seven Nation Army" },
+    { "sequence": ["D5", "D5", "D5", "E5", "D5", "D5"], "duration": [4, 2, 2, 2, 2, 2], song: "L, How a Rose E'er Blooming" },
+    { "sequence": ["A4", "A4", "G4", "F4", "D4", "A4"], "duration": [2, 2, 1, 1, 2, 1], song: "Rolling in the Deep" },
+    { "sequence": ["E4", "G4", "C5", "B4", "A4", "G4"], "duration": [1, 1, 3, 1, 8, 2], song: "Viva La Vida" },
+    { "sequence": ["A3", "C4", "D4", "D4", "D4", "E4"], "duration": [1, 1, 2, 2, 1, 1], song: "Pirates of the Caribbean" },
+    { "sequence": ["C4", "G4", "F4", "E4", "D4", "C5"], "duration": [3, 3, 1, 1, 1, 3], song: "Star Wars" },
+    { "sequence": ["A4", "A4", "G4", "G4", "G4", "G4"], "duration": [1, 1, 1, 1, 1, 1], song: "Somebody I Used to Know" },
+    { "sequence": ["C5", "A4", "F4", "F4", "G4", "A4"], "duration": [4, 2, 5, 1.5, 0.5, 1], song: "Sesame Street" },
+
+
 ]
