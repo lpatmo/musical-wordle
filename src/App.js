a5092773e49ba5610db2cf6e5e0c21efde987a4f
arturolei <artie.lei@gmail.com>
2022-04-07 19:35:08 -0700

    Added mock-data file

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import { playSequence } from './helpers/playMusic';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlay } from '@fortawesome/free-solid-svg-icons';
 
+/* To access mock data for validation testing
+ *  1) Uncomment the following for mock data for validation testing
+ *  2) Comment out above import from data.js
+*/
+//import {data} from './data/mock-data.js';
+
 
 function App() {
   const [answer, setAnswer] = useState();
---
+++ src/data/mock-data.js
@@ -0,0 +1,12 @@
+/*These sequences are for edge case testing*/
+
+
+export const data = [
+    { "sequence": ["B4", "B4", "B4", "B4", "B4", "A4"], 
+    "duration": [1, 1, 1, 1, 1, 1], 
+    song: "All The Same but One; suggested guess: FAFFFA" },
+    { "sequence": ["A4", "A4", "B4", "B4", "B4", "B4"], 
+    "duration": [1, 1, 1, 1, 1, 1], 
+    song: "The Two Dupes and Quad Damage Jam; suggested guess: caccac" },
+
+]
