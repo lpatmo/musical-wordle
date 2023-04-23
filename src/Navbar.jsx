0d74e3f64849f45cc76c088fcae4a3bde01816b9
lpatmo <lindapnotes@gmail.com>
2023-04-22 17:54:26 -0700

    new logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -6,11 +6,10 @@ import Modal from './Modal'
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     const storage = JSON.parse(localStorage.getItem("stats"));
-    console.log('storage!!!', Array.isArray(storage))
 
     return (
         <div className={styles.navbar}>
-            <h1>Ear Training Daily</h1>
+            <img src="/pitch_puzzle.svg" alt="Pitch Puzzle logo"/>
             <EqualizerIcon onClick={() => setIsOpen(true)} style={{ fontSize: 40 }} />
             {isOpen && <Modal handleClose={() => setIsOpen(false)}>{storage}</Modal>}
         </div>
