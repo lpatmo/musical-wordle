05f3b03b6a1f08aa96f620b29038456cdbea6d42
lpatmo <lindapnotes@gmail.com>
2023-05-01 22:14:43 -0700

    Change navbar bg color to purple and add transparent logo and instructions svg

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,8 +7,9 @@ function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
     return (
         <div className={styles.navbar}>
-            <img src="/pitchpuzzle_newlogo.svg" alt="Perfect Pitch Puzzle logo" />
-            <span className={styles.instructions}>Figure out the first few notes of the tune. You have 6 tries.</span>
+            <img src="/logo_transparent.svg" alt="Perfect Pitch Puzzle logo" className={styles.logo} />
+            {/* <span className={styles.instructions}>Figure out the first few notes of the tune. You have 6 tries.</span> */}
+            <img src="/instructions.svg" alt="Figure out the first 6 notes of the tune. You have 6 attempts." width="450"/>
             <EqualizerIcon onClick={() => setIsOpen(true)} style={{ fontSize: 40 }} />
             {isOpen && <ModalStats setIsOpen={setIsOpen} />}
         </div>
