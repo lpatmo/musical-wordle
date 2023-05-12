d8517355def052e2732273f919ee92d448c93e3f
Linda <linda.peng@alumni.duke.edu>
2023-05-11 20:19:10 -0700

    Add a countdown timer and dateFns library (#93)
    * Add a countdown timer and dateFns library
    
    * Remove console log
    
    * Line break on timer
    
    * Update logic so index is never -1. Also, use 'puzzle' instead of 'game'
    
    * CSS adjustments for the timer

---
+++ src/CountdownTimer.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import { differenceInSeconds, addDays, startOfDay } from 'date-fns';
+
+function CountdownTimer() {
+  const [countdown, setCountdown] = useState('');
+
+  useEffect(() => {
+    let now = new Date();
+    let startOfToday = startOfDay(now);
+    let startOfTomorrow = addDays(startOfToday, 1);
+    let diffInSeconds = differenceInSeconds(startOfTomorrow, now);
+
+    let intervalId = setInterval(() => {
+      diffInSeconds--;
+
+      if (diffInSeconds === 0) {
+        startOfTomorrow = addDays(startOfTomorrow, 1);
+        diffInSeconds = differenceInSeconds(startOfTomorrow, now);
+      }
+
+      let remainingSeconds = diffInSeconds % 60;
+      let remainingMinutes = Math.floor(diffInSeconds / 60) % 60;
+      let remainingHours = Math.floor(diffInSeconds / 3600);
+
+      let countdownTimer = `${remainingHours.toString().padStart(2, '0')}:${remainingMinutes.toString().padStart(2, '0')}:${remainingSeconds.toString().padStart(2, '0')}`;
+
+      setCountdown(countdownTimer);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return <div className="countdown"><span className="countdownClock">{countdown}</span> <br />until the next puzzle</div>;
+}
+
+export default CountdownTimer;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import EqualizerIcon from '@mui/icons-material/Equalizer'; import Grid from '@mui/material/Grid';
 import styles from './Navbar.module.css';
 import ModalStats from './ModalStats';
+import CountdownTimer from './CountdownTimer';
 
 function Navbar() {
     const [isOpen, setIsOpen] = useState(false);
@@ -9,9 +10,11 @@ function Navbar() {
         <div className={styles.navbar}>
             <img src="/logo_transparent.svg" alt="Perfect Pitch Puzzle logo" className={styles.logo} />
             {/* <span className={styles.instructions}>Figure out the first few notes of the tune. You have 6 tries.</span> */}
-            <img src="/instructions_transparent.svg" alt="Identity the first 6 notes of the tune. You have 6 tries." width="450"/>
-            <EqualizerIcon onClick={() => setIsOpen(true)} style={{ fontSize: 40 }} />
+            <img src="/instructions_transparent.svg" alt="Identity the first 6 notes of the tune. You have 6 tries." width="450" />
+                <EqualizerIcon onClick={() => setIsOpen(true)} style={{ fontSize: 40 }} />
+                <CountdownTimer />
             {isOpen && <ModalStats setIsOpen={setIsOpen} />}
+
         </div>
     )
 }
