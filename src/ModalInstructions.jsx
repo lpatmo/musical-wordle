f82c64203e00b559e02deb1d173a6255c7da8dd1
Linda <linda.peng@alumni.duke.edu>
2023-05-27 13:42:53 -0700

    Add modal that pops up w/ instructions if user is first-time visitor and has never played the game (#99)
    * Initial instructions
    
    * Instruction adjustments
    
    * Last CSS edits
    
    * Show instructions modal on page load if there are no localStorage stats and this is the first time a user is visiting

---
+++ src/ModalInstructions.jsx
@@ -0,0 +1,63 @@
+import styles from './ModalInstructions.module.css'
+import Modal from './Modal'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faPlay,
+} from "@fortawesome/free-solid-svg-icons";
+import AudiotrackIcon from '@mui/icons-material/Audiotrack';
+
+function ModalInstructions({ setIsOpen }) {
+    return (
+        <Modal handleClose={() => setIsOpen(false)} hideClose={true}>
+            <section className={styles.modalInstructions}>
+                <h2>How to play</h2>
+                <ul>
+                    <li>Play the tune and identity the first 6 notes in 6 tries.</li>
+                </ul>
+                <h3><AudiotrackIcon /> Tips</h3>
+                <ul>
+                    <li>Click on the <button
+                            className={styles.playButton}
+                            onClick={(e) => {
+                                e.preventDefault();
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faPlay} />
+                        </button> button to listen to your guess</li>
+                    <li>
+                        <div className={styles.exampleTiles}>
+                            <span>B</span>
+                            <span>B</span>
+                            <span className={styles.correct}>G</span>
+                            <span>F</span>
+                            <span>F</span>
+                            <span>F</span>
+                        </div>
+                        You've identified a correct note and it's in the correct spot.</li>
+                    <li>
+                        <div className={styles.exampleTiles}>
+                            <span>B</span>
+                            <span className={styles.misplaced}>G</span>
+                            <span>A</span>
+                            <span>F</span>
+                            <span>F</span>
+                            <span>F</span>
+                        </div>
+                        You've identified a correct note but it's in the wrong spot.</li>
+                    <li>      <div className={styles.exampleTiles}>
+                        <span>B</span>
+                        <span>B</span>
+                        <span>E</span>
+                        <span>F</span>
+                        <span className={styles.incorrect}>D</span>
+                        <span>D</span>
+                    </div>
+                        The note you've identified is not in the tune.</li>
+                </ul>
+                <p>A new musical puzzle is released daily at midnight.</p>
+            </section>
+        </Modal>
+    )
+}
+
+export default ModalInstructions;
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,20 +1,37 @@
-import { useState, useContext } from 'react';
-import EqualizerIcon from '@mui/icons-material/Equalizer'; import Grid from '@mui/material/Grid';
+import { useEffect, useState } from 'react';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+import InfoIcon from '@mui/icons-material/Info';
+import Grid from '@mui/material/Grid';
 import styles from './Navbar.module.css';
 import ModalStats from './ModalStats';
+import ModalInstructions from './ModalInstructions';
 import CountdownTimer from './CountdownTimer';
 
+
 function Navbar() {
-    const [isOpen, setIsOpen] = useState(false);
+    const [showStats, setShowStats] = useState(false);
+    const [showInstructions, setShowInstructions] = useState(false);
+    useEffect(() => {
+        const storage = JSON.parse(localStorage.getItem("perfectPitchPuzzleStats"));
+        let hasVisited = sessionStorage.getItem("perfectPitchPuzzleHasVisited");
+
+        if (!storage && !hasVisited) {
+            setShowInstructions(true);
+            sessionStorage.setItem("perfectPitchPuzzleHasVisited", "true");
+        }
+    }, [])
 
     return (
         <div className={styles.navbar}>
             <img src="/logo_transparent.svg" alt="Perfect Pitch Puzzle logo" className={styles.logo} />
             {/* <span className={styles.instructions}>Figure out the first few notes of the tune. You have 6 tries.</span> */}
             <img src="/instructions_transparent.svg" alt="Identity the first 6 notes of the tune. You have 6 tries." width="450" />
-                <EqualizerIcon onClick={() => setIsOpen(true)} style={{ fontSize: 40 }} />
-                <CountdownTimer />
-            {isOpen && <ModalStats setIsOpen={setIsOpen} />}
+
+            <InfoIcon onClick={() => setShowInstructions(true)} style={{ fontSize: 40 }} />
+            <EqualizerIcon onClick={() => setShowStats(true)} style={{ fontSize: 40 }} />
+            <CountdownTimer />
+            {showStats && <ModalStats setIsOpen={setShowStats} />}
+            {showInstructions && <ModalInstructions setIsOpen={setShowInstructions} />}
 
         </div>
     )
