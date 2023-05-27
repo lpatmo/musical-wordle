5ed1ebfb6901188c4cc56553180ca0ffa78be5fc
lpatmo <lindapnotes@gmail.com>
2023-05-26 19:29:32 -0700

    Instruction adjustments

--- src/ModalInstructions.jsx
+++ src/ModalInstructions.jsx
@@ -12,23 +12,46 @@ function ModalInstructions({ setIsOpen }) {
                 <h2>How to play</h2>
                 <ul>
                     <li>Play the tune and identity the first 6 notes in 6 tries.</li>
-                    <li>You have 6 tries.</li>
                 </ul>
                 <h3>Tips</h3>
                 <ul>
-                    <li>Click on the                   <button
-                        className={styles.playButton}
-                        onClick={(e) => {
-                            e.preventDefault();
-                        }}
-                    >
-                        <FontAwesomeIcon icon={faPlay} />
-                    </button> button to listen to your guess</li>
-                    <li>Hit 'enter' or 'submit' to submit a guess</li>
-                    <li><div className={`${styles.exampleTile} ${styles.correct}`}></div> – you've identified a correct note and it's in the correct spot.</li>
-                    <li><div className={`${styles.exampleTile} ${styles.misplaced}`}></div> – you've identified a correct note and it's in the wrong spot.</li>
-                    <li><div className={`${styles.exampleTile} ${styles.incorrect}`}></div> – the note you've identified is not in the solution.</li>
-
+                    <li>Click on the <button
+                            className={styles.playButton}
+                            onClick={(e) => {
+                                e.preventDefault();
+                            }}
+                        >
+                            <FontAwesomeIcon icon={faPlay} />
+                        </button> button to listen to your guess</li>
+                    <li>
+                        <div className={styles.exampleTiles}>
+                            <span>B</span>
+                            <span>B</span>
+                            <span className={styles.correct}>G</span>
+                            <span>F</span>
+                            <span>F</span>
+                            <span>F</span>
+                        </div>
+                        You've identified a correct note and it's in the correct spot.</li>
+                    <li>
+                        <div className={styles.exampleTiles}>
+                            <span>B</span>
+                            <span className={styles.misplaced}>G</span>
+                            <span>A</span>
+                            <span>F</span>
+                            <span>F</span>
+                            <span>F</span>
+                        </div>
+                        You've identified a correct note but it's in the wrong spot.</li>
+                    <li>      <div className={styles.exampleTiles}>
+                        <span>B</span>
+                        <span>B</span>
+                        <span>D</span>
+                        <span>F</span>
+                        <span className={styles.incorrect}>D</span>
+                        <span>D</span>
+                    </div>
+                        The note you've identified is not in the tune.</li>
                 </ul>
                 <p>A new musical puzzle is released daily at midnight.</p>
             </section>
