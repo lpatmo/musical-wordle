ee1069914ffe2f3732414e57c46c8d91a53b07fb
lpatmo <lindapnotes@gmail.com>
2023-07-14 16:17:54 -0700

    Put labels on black keys

--- src/PianoNew.jsx
+++ src/PianoNew.jsx
@@ -7,16 +7,16 @@ function PianoNew({ handlePianoPress, octave }) {
     return (<div className={styles.pianoNew}>
         <ul className={styles.keyboard}>
             <li onClick={() => handlePianoPress("C")} note="C" className={styles.white}>C{octave}</li>
-            <li onClick={() => handlePianoPress("C#")} note="C#" className={styles.black}></li>
+            <li onClick={() => handlePianoPress("C#")} note="C#" className={styles.black}>C#</li>
             <li onClick={() => handlePianoPress("D")} note="D" className={`${styles.white} ${styles.offset}`}>D{octave}</li>
-            <li note="D#" className={styles.black}></li>
+            <li note="D#" className={styles.black}>D#</li>
             <li onClick={() => handlePianoPress("E")} note="E" className={`${styles.white} ${styles.offset}`}>E{octave}</li>
             <li onClick={() => handlePianoPress("F")} note="F" className={styles.white}>F{octave}</li>
-            <li note="F#" className={styles.black}></li>
+            <li note="F#" className={styles.black}>F#</li>
             <li onClick={() => handlePianoPress("G")} note="G" className={`${styles.white} ${styles.offset}`}>G{octave}</li>
-            <li note="A#" className={styles.black}></li>
+            <li note="G#" className={styles.black}>G#</li>
             <li onClick={() => handlePianoPress("A")} note="A" className={`${styles.white} ${styles.offset}`}>A{octave}</li>
-            <li note="A#" className={styles.black}></li>
+            <li note="A#" className={styles.black}>A#</li>
             <li onClick={() => handlePianoPress("B")} note="B" className={`${styles.white} ${styles.offset}`}>B{octave}</li>
             <li aria-label="backspace" className={`${styles.white} ${styles.backspace}`} onClick={() => handlePianoPress("Backspace")}> <span>Backspace</span> <FontAwesomeIcon icon={faBackspace} /></li>
         </ul>
