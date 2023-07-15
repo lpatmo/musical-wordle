ded209320f4c42489d4441fdabbc015d5c3ecc9a
lpatmo <lindapnotes@gmail.com>
2023-07-14 22:47:22 -0700

    handle ability to click sharps

--- src/PianoNew.jsx
+++ src/PianoNew.jsx
@@ -9,12 +9,12 @@ function PianoNew({ handlePianoPress, octave }) {
             <li onClick={() => handlePianoPress("C")} note="C" className={styles.white}>C{octave}</li>
             <li onClick={() => handlePianoPress("C#")} note="C#" className={styles.black}>C#</li>
             <li onClick={() => handlePianoPress("D")} note="D" className={`${styles.white} ${styles.offset}`}>D{octave}</li>
-            <li note="D#" className={styles.black}>D#</li>
+            <li onClick={() => handlePianoPress("D#")} note="D#" className={styles.black}>D#</li>
             <li onClick={() => handlePianoPress("E")} note="E" className={`${styles.white} ${styles.offset}`}>E{octave}</li>
             <li onClick={() => handlePianoPress("F")} note="F" className={styles.white}>F{octave}</li>
-            <li note="F#" className={styles.black}>F#</li>
+            <li onClick={() => handlePianoPress("F#")} note="F#" className={styles.black}>F#</li>
             <li onClick={() => handlePianoPress("G")} note="G" className={`${styles.white} ${styles.offset}`}>G{octave}</li>
-            <li note="G#" className={styles.black}>G#</li>
+            <li onClick={() => handlePianoPress("G#")} note="G#" className={styles.black}>G#</li>
             <li onClick={() => handlePianoPress("A")} note="A" className={`${styles.white} ${styles.offset}`}>A{octave}</li>
             <li note="A#" className={styles.black}>A#</li>
             <li onClick={() => handlePianoPress("B")} note="B" className={`${styles.white} ${styles.offset}`}>B{octave}</li>
