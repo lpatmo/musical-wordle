cfb642de7023db696ebdb0deaafecdb015ffd3d7
lpatmo <lindapnotes@gmail.com>
2023-07-19 01:20:45 -0700

    Add tiny piano

---
+++ src/PianoTiny.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from "./PianoTiny.module.css";
+
+function PianoTiny({ handlePianoPress, octave, hasFlats }) {
+    return (<div className={styles.pianoNew}>
+        <ul className={styles.keyboard}>
+            <li className={styles.white}></li>
+            <li className={styles.black}></li>
+            <li className={`${styles.white} ${styles.offset}`}></li>
+            <li className={styles.black}></li>
+            <li className={`${styles.white} ${styles.offset}`}></li>
+            <li className={styles.white}></li>
+            <li className={styles.black}></li>
+            <li className={`${styles.white} ${styles.offset}`}></li>
+            <li className={styles.black}></li>
+            <li className={`${styles.white} ${styles.offset}`}></li>
+            <li className={styles.black}></li>
+            <li className={`${styles.white} ${styles.offset}`}></li>
+        </ul>
+    </div>)
+}
+
+export default PianoTiny;
