a34654dea2de23e03a749dbd78776d84d34a8eba
lpatmo <lindapnotes@gmail.com>
2023-09-11 22:40:42 -0700

    key highlight

--- src/Board.jsx
+++ src/Board.jsx
@@ -194,12 +194,39 @@ function Board({ answer, testMode }) {
         case isNote(event.key):
           /*Update guess state after valid note*/
           let note = getNote(event, answer?.hasFlats);
-          //console.log("note", note)
+     
           if (note[1] === "b") {
             note = note[0].toUpperCase() + note[1];
           } else {
             note = note.toUpperCase();
           }
+
+          //console.log("note", note)
+          //highlight piano keyboard notes
+          setTimeout(() => {
+            if (note[1] === '.') {
+              document.querySelector(`#${note[0]}`).classList.add('activeWhite')
+            } else {
+              if (answer?.hasFlats) {
+                document.querySelector(`#${note[0]}flat`).classList.add('activeBlack')
+              } else {
+                document.querySelector(`#${note[0]}sharp`).classList.add('activeBlack')
+              }
+            }
+          }, 0)
+
+          setTimeout(() => {
+            if (note[1] === '.') {
+              document.querySelector(`#${note[0]}`).classList.remove('activeWhite')
+            } else {
+              if (answer?.hasFlats) {
+                document.querySelector(`#${note[0]}flat`).classList.remove('activeBlack')
+              } else {
+                document.querySelector(`#${note[0]}sharp`).classList.remove('activeBlack')
+              }
+            }
+          }, 200)
+  
           if (guess[currentRow].length < 12) {
             setGuess(
               guess.map((guessNote, i) => {
